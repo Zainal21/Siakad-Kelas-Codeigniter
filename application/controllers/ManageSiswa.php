fbc8835ec5fcc18924f92a37e2fe5eb609455a15
Zainal21 <Zainalarifin080718@gmail.com>
2020-02-22 20:34:37 +0700

    fix Login MultiLevel

--- application/controllers/ManageSiswa.php
+++ application/controllers/ManageSiswa.php
@@ -38,49 +38,4 @@ class ManageSiswa extends CI_Controller{
 
 
 }
-
-
-=======
-<?php 
-defined('BASEPATH') OR exit('dilarang mengakses file ini');
-class ManageSiswa extends CI_Controller{
-    
-    public function __construct(){
-        parent::__construct();
-        $this->load->model('M_Siswa');
-        $this->load->library('form_validation');
-        
-    }
-    public function index(){
-        $data['siswa'] = $this->M_Siswa->allsiswa();
-        $this->load->view('BackEnd/Master/ManageSiswa', $data);
-    }
-    public function TambahSiswa()
-    {
-        $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('nis', 'nis', 'required');
-        $this->form_validation->set_rules('kelas', 'Kelas', 'required');
-       
-      if($this->form_validation->run()== FALSE){
-          $data['kelas'] = $this->M_Siswa->FetchData();
-           $this->load->view('BackEnd/FormTambah/TambahManageSiswa', $data);
-      }else{
-          $this->M_Siswa->Add();
-          $this->session->set_flashdata('Data', 'Berhasil Ditambahkan');
-          redirect('ManageSiswa');
-      }
-    }
-    public function delete($id)
-    {
-        $data = $this->M_Siswa->Delete($id);
-        json_encode($data);
-        $this->session->set_flashdata('Data', 'Berhasil Ditambahkan');
-        redirect('ManageSiswa');
-    }
-
-
-}
-
-
->>>>>>> d218089b615ef190f2762115b1e7cb640c306826
 ?>
--- application/models/M_Siswa.php
+++ application/models/M_Siswa.php
@@ -39,61 +39,10 @@ class M_Siswa extends CI_Model{
       $this->db->where('id', $id);
       $this->db->update('siswa', $data);
     }
-<<<<<<< HEAD
-=======
-
 
 }
 
 
 
-=======
-<?php 
-defined('BASEPATH') OR exit('dilarang mengakses file ini');
-class M_Siswa extends CI_Model{
 
- public function allsiswa(){
-   $this->db->order_by('nis','ASC');
-    return $this->db->from('siswa')
-    ->join('kelas', 'siswa.kode_kelas=kelas.kode_kelas')->get()->result_array();
-    
- }
-  public function FetchData()
-    {
-      return $this->db->get('kelas')->result_array();
-    }
-
-    public function delete($id)
-    {
-      $this->db->delete('siswa', ['id'=> $id]);
-    }
-    public function Add()
-    {
-      $data = [
-        'id' => $this->input->post('id'),
-        'nis' => $this->input->post('nis'),
-        'nama' => $this->input->post('nama'),
-        'kode_kelas' => $this->input->post('kelas')
-      ];
-      $this->db->insert('siswa', $data);
-    }
-    public function update($id)
-    {
-        $data = [
-        'id' => $this->input->post('id'),
-        'nis' => $this->input->post('nis'),
-        'nama' => $this->input->post('nama'),
-        'kode_kelas' => $this->input->post('kelas')
-      ];
-      $this->db->where('id', $id);
-      $this->db->update('siswa', $data);
-    }
->>>>>>> a97929e791a6ae828ed899667dad6c7189cc6d71
-
-
-}
-
-
-
->>>>>>> d218089b615ef190f2762115b1e7cb640c306826
 ?>
